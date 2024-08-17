07fac65ad338fa2dc1b9e9fcc1626ad80cd39532
SebitCaroca <sebastian.caroca@usach.cl>
2024-08-17 19:45:26 -0400

    "Removiendo conflictos de commit."

--- api/src/main/java/com/inmogo/api/ApiApplication.java
+++ api/src/main/java/com/inmogo/api/ApiApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class })
 public class ApiApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
 }
