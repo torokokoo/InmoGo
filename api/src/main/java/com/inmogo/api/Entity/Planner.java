f9b050a64f5b1b29eb119195d681dba049cdf66c
SebitCaroca <sebastian.caroca@usach.cl>
2024-08-26 17:52:39 -0400

    AAAAAAAAAAAAAAAAAAAAA

--- api/src/main/java/com/inmogo/api/Entity/Planner.java
+++ api/src/main/java/com/inmogo/api/Entity/Planner.java
@@ -5,7 +5,7 @@ public class Planner {
     private int idPlanner;
     private int idUserT;
     private int idListing;
-    private boolean ArrayPlanner[][];
+    private boolean[][] ArrayPlanner;
 
     //Constructor Planner
     public Planner(int idPlanner, int idUserT, int idListing, boolean[][] arrayPlanner) {
@@ -19,27 +19,20 @@ public class Planner {
 
     //get id of planner
     public int getIdPlanner() {return idPlanner;}
-
     //get id associeted to UserTemplate
     public int getIdUserT() {return idUserT;}
-
     //get id associeted to Listing
     public int getIdListing() {return idListing;}
-
     //get the appointment date of the User
     public boolean[][] getArrayPlanner() {return ArrayPlanner;}
 
     //setters Planner
-
     //change Id of Planner
     public void setIdPlanner(int idPlanner) {this.idPlanner = idPlanner;}
-
     //change Id of UserT
     public void setIdUserT(int idUserT) {this.idUserT = idUserT;}
-
     //change Id of Listing
     public void setIdListing(int idListing) {this.idListing = idListing;}
-
     //change the planner of UserT
     public void setArrayPlanner(boolean[][] arrayPlanner) {this.ArrayPlanner = arrayPlanner;}
 }
