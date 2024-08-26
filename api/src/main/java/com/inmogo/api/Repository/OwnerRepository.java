028b1872d2763dd8788e075877a0095c5115dc82
SebitCaroca <sebastian.caroca@usach.cl>
2024-08-26 16:31:34 -0400

    a

--- api/src/main/java/com/inmogo/api/Repository/OwnerRepository.java
+++ api/src/main/java/com/inmogo/api/Repository/OwnerRepository.java
@@ -1,9 +1,9 @@
 package com.inmogo.api.Repository;
 
-import com.inmogo.api.Entity.UserOwner;
+import com.inmogo.api.Entity.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OwnerRepository extends JpaRepository<UserOwner, Long>  {
+public interface OwnerRepository extends JpaRepository<Owner, Long>  {
 }
--- api/src/main/java/com/inmogo/api/Repository/UserRepository.java
+++ api/src/main/java/com/inmogo/api/Repository/UserRepository.java
@@ -3,11 +3,10 @@ package com.inmogo.api.Repository;
 import com.inmogo.api.Entity.UserTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserTemplate, Long> {
-    Optional<UserTemplate> findUserById(Long id);
-    Optional<UserTemplate> findUserByEmail(String email);
-    Optional<UserTemplate> findUserByRut(String rut);
+    UserTemplate findUserById(Long id);
+    UserTemplate findUserByEmail(String email);
+    UserTemplate findUserByRut(String rut);
 }
