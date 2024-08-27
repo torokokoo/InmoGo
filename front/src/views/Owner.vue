b20b70edcd508d3c481ce035cd712e2fe754c478
agustinsaavedra2 <agustin.saavedra@usach.cl>
2024-08-27 00:42:37 -0400

    Se implementa lógica en las propiedades que tiene el Owner en su vista.

--- front/src/views/Owner.vue
+++ front/src/views/Owner.vue
@@ -1,12 +1,20 @@
 <template>
-    
+    <div>
+        <h2>Propiedades</h2>
+        <br>
+        <ul>
+            <li v-for="(listings, index) in listings" :key="index">
+                {{  listings }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
     import { mapActions } from 'vuex';
     export default{
         data: ()=>({
-            listings: [],
+            listings: ["Casa en la Playa", "Edificio Centrico", "Casa arrendada"],
             selectedHours: {
                 lunes: [],
                 martes: [],
