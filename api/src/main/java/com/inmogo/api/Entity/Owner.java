c82f706c1685a79738359f8f702f27d9bf035e4b
agustinsaavedra2 <agustin.saavedra@usach.cl>
2024-08-29 22:38:24 -0400

    Arreglo error en backend

--- api/src/main/java/com/inmogo/api/Entity/Owner.java
+++ api/src/main/java/com/inmogo/api/Entity/Owner.java
@@ -7,23 +7,7 @@ import jakarta.persistence.*;
 @Entity
 @DiscriminatorValue("Owner")
 public class Owner extends UserTemplate {
-    private List<Listing> listings = new ArrayList<>();
     
-     public Listing publish(String title, String notesFromPropietary, int rankingPos) {
-        Listing newListing = new Listing(title, new Timestamp(System.currentTimeMillis()), notesFromPropietary, rankingPos);
-        listings.add(newListing);
-        
-        return newListing;
-    }
-
-    public Listing editPublish(Listing listing, String newTitle, String newNotes, int newRankingPos){
-        listing.setTitle(newTitle);
-        listing.publishDate(new Timestamp(System.currentTimeMillis()));
-        listing.setNotesFromPropietary(newNotes);
-        listing.setRankingPos(newRankingPos);
-        
-        return listing;
-    }	
 
     public Owner(long id, String name, String rut, String email, String password) {
         super(id, name, rut, email, password, 3);
