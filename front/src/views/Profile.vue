f6dfa5e9aefec5b3d4a05d63fda8f3683ea3fc01
agustinsaavedra2 <agustin.saavedra@usach.cl>
2024-08-29 21:55:29 -0400

    Vista de perfil de usuario desarrollada.

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -1,6 +1,16 @@
 <template>
-  <Navbar />
-  <button @click="logout">Logout</button>
+  <div>
+    <Navbar />
+    <div class="container mx-auto p-6">
+      <div class="heading text-center font-bold text-3xl mb-8 text-gray-800">Perfil de Usuario</div>
+      <div class="flex justify-around mb-8">
+        <button class="btn bg-indigo-500 text-white hover:bg-teal-500">Visitas Agendadas</button>
+        <button class="btn bg-indigo-500 text-white hover:bg-teal-500">Chat de Mensajería</button>
+        <button class="btn bg-indigo-500 text-white hover:bg-teal-500">Contactar Soporte</button>
+        <button class="btn bg-red-500 text-white hover:bg-red-600">Logout</button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
   import Navbar from '@/components/Navbar';
