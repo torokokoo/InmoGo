71c9505e113d75e3274c5bacb81497e52b1870eb
torokokoo <torokokov7@gmail.com>
2024-09-02 10:02:30 -0400

    hotfix: roles

--- front/src/views/Profile.vue
+++ front/src/views/Profile.vue
@@ -6,7 +6,7 @@
     <div class="container mx-auto p-6">
       <div class="heading text-center font-bold text-3xl mb-8 text-gray-800">Datos de la cuenta</div>
       <p class="text-center"><span class="font-bold">Nombre:</span>{{ user.name }}</p>
-      <p class="text-center"><span class="font-bold">Rol:</span>{{ role }}</p>
+      <p class="text-center"><span class="font-bold">Rol:</span>{{ roles[user.role] }}</p>
       <p class="text-center"><span class="font-bold">Email:</span>{{ user.email }}</p>
     </div>
   </div>
@@ -26,23 +26,11 @@ export default {
   data() {
     return {
       user: {},
-      role: '',
+      roles: ['Visitante', 'Administrador', 'Mantenedor', 'Propietario', 'Adquiriente'],
     };
   },
   mounted() {
     this.user = JSON.parse(localStorage.getItem('user'))
-    switch (this.user.role) {
-      case 1:
-        this.role = 'Administrador' 
-      case 2:
-        this.role = 'Mantenedor'
-      case 3:
-        this.role = 'Propietario'
-      case 4:
-        this.role = 'Adquiriente'
-      default:
-        break;
-    }
   },
   methods: {
   },
